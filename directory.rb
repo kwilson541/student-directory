8c4824837711f3a2408018538de10162e8f7a7f6
Kim Wilson <k.wilson541@gmail.com>
2016-10-27 14:52:16 +0100

    Fixed bug in print by cohort function

--- directory.rb
+++ directory.rb
@@ -1,21 +1,10 @@
+# An empty array accessible to all methods
+@students = []
+
 def interactive_menu
-  # An empty array accessible to all methods
-  @students = []
   loop do
     print_menu
-    # Read the input and save it into a variable
-    selection = gets.chomp
-    # 3. Do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      show_students
-    when "9"
-      exit # This will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    process(gets.chomp)
   end
 end
 
@@ -23,14 +12,30 @@ def print_menu
   # Print the menu and ask the user what to do
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Print by Cohort"
   # 9 Because we'll be adding more items
   puts "9. Exit"
 end
 
 def show_students
   print_header
-  print
+  print_students_list
   print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      show_students
+    when "3"
+      print_by_cohort
+    when "9"
+      exit # This will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
 end
 
 def input_students
@@ -73,8 +78,8 @@ def print_header
   puts "-------------".center(30)
   end
 end
-
-def print
+ 
+def print_students_list
   count = 0
   until count == @students.length
     puts "Name: #{@students[count][:name]}, Origin: #{@students[count][:origin]}, DOB: #{@students[count][:dob]}, Cohort: #{@students[count][:cohort]}"
@@ -131,7 +136,7 @@ def print_by_cohort
   cohort_input = gets.chomp.capitalize.to_sym
   @students.each { |student|
     if student[:cohort] == cohort_input
-    puts "Name: #{@student[:name]}, Origin: #{@student[:origin]}, DOB: #{@student[:dob]}"
+    puts "Name: #{student[:name]}, Origin: #{student[:origin]}, DOB: #{student[:dob]}"
   end
 }
 end
