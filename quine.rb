6330c2fab9c021bfda487e591b9bd481ad0e1a2f
Kim Wilson <k.wilson541@gmail.com>
2016-10-30 12:09:12 +0000

    Refactored quine

--- quine.rb
+++ quine.rb
@@ -1,9 +1,5 @@
-def quine
-	file = File.open($0, "r")
- 	file.readlines.each do |line|
- 	puts line
- 	end
- 	file.close
+File.open($0, "r") { |file| 
+file.readlines.each do |line|
+puts line
 end
-
-quine
+}
